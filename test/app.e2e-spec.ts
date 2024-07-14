3816805123024f346f9877f8bf8ad9d64f5b3a6e
Iqbal Pamula <balpamula@gmail.com>
2024-07-14 17:33:39 +0700

    e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -20,5 +20,14 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('should say hello', async () => {
+    const result = await request(app.getHttpServer())
+      .get('/v1/hello')
+      .query({ firstName: 'Iqbal', lastName: 'Pamula' });
+
+    expect(result.status).toBe(200);
+    expect(result.body.message).toBe('Hello Iqbal Pamula');
   });
 });
