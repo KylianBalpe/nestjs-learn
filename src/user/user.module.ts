491b3c51b7855cf2728869ec3253c3bf43c80239
Iqbal Pamula <balpamula@gmail.com>
2024-07-22 22:15:56 +0700

    Global Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserController } from '@/user/user.controller';
 import { UserService } from '@/user/user.service';
 import { UserRepository } from '@/user/user-repository/user-repository';
-import { PrismaModule } from '@/prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule],
+  // imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
